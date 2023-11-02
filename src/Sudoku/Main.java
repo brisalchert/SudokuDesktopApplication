26927df476cc7dc5fcc63ea412b1fb78bfa5488a
Brian Salchert <brisalchert@gmail.com>
2023-11-02 18:00:05 -0400

    Added User Interface to start method

--- src/Sudoku/Main.java
+++ src/Sudoku/Main.java
@@ -1,18 +1,14 @@
 package Sudoku;
 
+import Sudoku.UserInterface.UserInterface;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         try {
-            BorderPane root = new BorderPane();
-            Scene scene = new Scene(root, 400, 400);
-            primaryStage.setScene(scene);
-            primaryStage.show();
+            UserInterface userInterface = new UserInterface(primaryStage);
         }
         catch (Exception error) {
             error.printStackTrace();
