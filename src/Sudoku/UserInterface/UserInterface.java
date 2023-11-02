dab0b7444d8fa7b5c5103dec23f5059620d5d425
Brian Salchert <brisalchert@gmail.com>
2023-11-02 17:12:27 -0400

    Added Sudoku grid tiles

--- src/Sudoku/UserInterface/UserInterface.java
+++ src/Sudoku/UserInterface/UserInterface.java
@@ -4,12 +4,14 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
-import javafx.scene.text.TextBoundsType;
 import javafx.stage.Stage;
 
 public class UserInterface {
@@ -18,7 +20,8 @@ public class UserInterface {
     private final int BOARD_WIDTH_AND_HEIGHT = 576;
     private final int WINDOW_WIDTH = 676;
     private final int WINDOW_HEIGHT = 776;
-    private final Color backgroundColor = Color.rgb(168, 136, 98, 1);
+    private final Color WINDOW_BACKGROUND_COLOR = Color.rgb(168, 136, 98, 1);
+    private final Color TILE_BACKGROUND_COLOR = Color.WHEAT;
 
     public UserInterface(Stage stage) {
         this.stage = stage;
@@ -27,21 +30,26 @@ public class UserInterface {
     }
 
     private void initializeUserInterface() {
+        VBox sudokuElements = new VBox();
+        sudokuElements.setAlignment(Pos.CENTER);
+
         drawBackground(root);
-        drawTitle(root);
-        drawSudokuBoard(root);
-        drawGridLines(root);
+        drawTitle(sudokuElements);
+        drawSudokuBoard(sudokuElements);
+        drawGridLines(sudokuElements);
+
+        root.getChildren().add(sudokuElements);
         stage.show();
     }
 
     private void drawBackground(Group root) {
         Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
-        scene.setFill(backgroundColor);
+        scene.setFill(WINDOW_BACKGROUND_COLOR);
         stage.setTitle("Sudoku");
         stage.setScene(scene);
     }
 
-    private void drawTitle(Group root) {
+    private void drawTitle(Pane root) {
         HBox titleBox = new HBox();
         Text title = new Text("Sudoku");
         Font titleFont = new Font("Century", 50);
@@ -54,11 +62,50 @@ public class UserInterface {
         root.getChildren().add(titleBox);
     }
 
-    private void drawSudokuBoard(Group root) {
+    private void drawSudokuBoard(Pane root) {
+        GridPane boxGrid = new GridPane(100, 100);
+        int boxGridWidthAndHeight = 3;
+
+        boxGrid.setAlignment(Pos.CENTER);
+
+        // Set spacing between boxes to 3 pixels
+        boxGrid.setHgap(3);
+        boxGrid.setVgap(3);
+
+        // Create a 3x3 grid of boxes
+        for (int row = 0; row < boxGridWidthAndHeight; row++) {
+            for (int column = 0; column < boxGridWidthAndHeight; column++) {
+                drawSudokuBox(boxGrid, row, column);
+            }
+        }
+
+        // Add the boxGrid to the board
+        root.getChildren().add(boxGrid);
+    }
+
+    private void drawGridLines(Pane root) {
 
     }
 
-    private void drawGridLines(Group root) {
+    // Indices will be used for alternating tile colors
+    private void drawSudokuBox(GridPane root, int xIndex, int yIndex) {
+        GridPane box = new GridPane();
+        int boxWidthAndHeight = 3;
 
+        // Set spacing between tiles to 2 pixels
+        box.setHgap(2);
+        box.setVgap(2);
+
+        // Create one 3x3 box of 9 tiles
+        for (int row = 0; row < boxWidthAndHeight; row++) {
+            for (int column = 0; column < boxWidthAndHeight; column++) {
+                Rectangle tile = new Rectangle(64, 64);
+                tile.setFill(TILE_BACKGROUND_COLOR);
+                box.add(tile, row, column);
+            }
+        }
+
+        // Add the box to the boxGrid
+        root.add(box, xIndex, yIndex);
     }
 }
