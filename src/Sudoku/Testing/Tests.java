c0b4538468485e4410ffc86945569006334de404
Brian Salchert <brisalchert@gmail.com>
2024-03-21 12:35:46 -0400

    Added generateGrids() test method

--- src/Sudoku/Testing/Tests.java
+++ src/Sudoku/Testing/Tests.java
@@ -3,6 +3,9 @@ package Sudoku.Testing;
 import Sudoku.GameLogic.PuzzleGenerator;
 import Sudoku.UserInterface.SudokuTile;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class Tests {
@@ -25,5 +28,73 @@ public class Tests {
 
         // Set the candidates for the invalidated tile to the random candidate
         tileToInvalidate.setCandidates(Integer.toString(candidate));
+    }
+
+    /**
+     * Generates a certain number of Sudoku grids and reports back the minimum, maximum, and average runtimes
+     * @param numGrids the number of Sudoku grids to generate
+     */
+    public static void generateGrids(int numGrids) {
+        ArrayList<Long> generationTimes = new ArrayList<>(numGrids);
+        DecimalFormat twoPlaces = new DecimalFormat("0.00");
+        long minimum;
+        long maximum;
+        double average;
+
+        for (int i = 0; i < numGrids; i++) {
+            long startTime;
+            long endTime;
+            long runtime;
+
+            // Empty the grid
+            emptyTileGrid();
+
+            // Record start time
+            startTime = System.nanoTime();
+
+            // Generate a grid
+            PuzzleGenerator puzzle = new PuzzleGenerator();
+
+            // Record end time
+            endTime = System.nanoTime();
+
+            // Calculate runtime
+            runtime = endTime - startTime;
+
+            // Add the runtime to the ArrayList of generation times
+            generationTimes.add(runtime);
+        }
+
+        // Calculate statistics
+        minimum = Collections.min(generationTimes);
+        maximum = Collections.max(generationTimes);
+
+        long sum = 0;
+
+        for (Long time : generationTimes) {
+            sum += time;
+        }
+
+        average = (double) sum / generationTimes.size();
+
+        System.out.println("STATISTICS FOR GENERATION OF " + numGrids + " GRIDS:");
+        System.out.println("------------------------------------------------------------");
+        System.out.println("- Minimum time: " + twoPlaces.format(minimum / 1000000.0) + " ms");
+        System.out.println("- Maximum time: " + twoPlaces.format(maximum / 1000000.0) + " ms");
+        System.out.println("- Average time: " + twoPlaces.format(average / 1000000.0) + " ms");
+    }
+
+    /**
+     * Removes all values in the tileGrid, setting them to null
+     */
+    private static void emptyTileGrid() {
+        SudokuTile[][] tileGrid = SudokuTile.getTileGrid();
+
+        // Remove all values
+        for (SudokuTile[] column : tileGrid) {
+            for (SudokuTile tile : column) {
+                tile.setValue(null);
+            }
+        }
     }
 }
